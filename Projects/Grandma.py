feada9ffdd820b66cee0c1f657ed196c9e8f5f33
Xwtheshifts <xavieravril@seattleacademy.org>
2025-03-28 21:01:40 +0000

    Project 2

---
+++ Projects/Grandma.py
@@ -0,0 +1,7 @@
+while True:
+    word = input("what do you think grandma likes?")
+    if "a" in word:
+        print(f"Grandma doesn't like {word}!")
+    else:
+        print(f"Grandma likes {word}")
+    print("")
